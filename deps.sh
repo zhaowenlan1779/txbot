15a90e1435f700d6ca2f5d106cf6020a07b6c462
Pengfei <zhupf321@gmail.com>
2021-10-16 14:18:28 +0800

    Remove writing tx

--- deps.sh
+++ deps.sh
@@ -9,15 +9,3 @@ hostname = https://www.transifex.com
 username = api
 password = $TRANSIFEX_API_TOKEN
 EOF
-
-# Not sure if this is necessary, but whatever
-cat << 'EOF' > /usr/bin/tx
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-import re
-import sys
-from txclib.cmdline import main
-if __name__ == '__main__':
-    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
-    sys.exit(main())
-EOF
