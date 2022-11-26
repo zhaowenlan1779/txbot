378c3063700e32b693d219a1d2b0f156bd82261a
Pengfei Zhu <zhupf321@gmail.com>
2022-11-26 23:50:03 +0800

    Use env var

--- deps.sh
+++ deps.sh
@@ -4,11 +4,3 @@ git config --global user.email "50136859+zhaobot@users.noreply.github.com"
 git config --global user.name "zhaobot"
 
 apt install -y ruby
-
-# Setup RC file for tx
-cat << EOF > ~/.transifexrc
-[https://www.transifex.com]
-hostname = https://www.transifex.com
-username = api
-password = $TRANSIFEX_API_TOKEN
-EOF
