f2c1ae308f4adbf8324f236a554b7b85de27c462
Pengfei Zhu <zhupf321@gmail.com>
2022-11-26 23:44:18 +0800

    fix

--- deps.sh
+++ deps.sh
@@ -3,7 +3,7 @@
 git config --global user.email "50136859+zhaobot@users.noreply.github.com"
 git config --global user.name "zhaobot"
 
-apt install ruby
+apt install -y ruby
 
 # Setup RC file for tx
 cat << EOF > ~/.transifexrc
