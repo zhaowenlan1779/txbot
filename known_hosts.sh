e6de76852c23de32b519638466ebad7b59bc2bee
Pengfei Zhu <zhupf321@gmail.com>
2023-04-04 13:12:40 +0800

    Test

--- known_hosts.sh
+++ known_hosts.sh
@@ -2,8 +2,7 @@
 
 apt-get install -y jq
 
-echo "Home directory is $HOME"
-mkdir -p -m 0700 ~/.ssh
-curl --silent https://api.github.com/meta  | jq --raw-output '"github.com "+.ssh_keys[]' > ~/.ssh/known_hosts
-chmod 600 ~/.ssh/known_hosts
-cat ~/.ssh/known_hosts
+mkdir -p -m 0700 /root/.ssh
+curl --silent https://api.github.com/meta  | jq --raw-output '"github.com "+.ssh_keys[]' > /root/.ssh/known_hosts
+chmod 600 /root/.ssh/known_hosts
+cat /root/.ssh/known_hosts
